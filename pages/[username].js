c49af3b287a189a5081aa6c699152be4fb1ffda4
samirrh <samirhaque808@gmail.com>
2021-11-14 02:43:08 -0500

    getting links by user

--- pages/[username].js
+++ pages/[username].js
@@ -1,10 +1,31 @@
 import { useRouter } from 'next/router';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const Post = () => {
   const router = useRouter();
   const { username } = router.query;
+  const [res, setRes] = useState([]);
 
-  return <p>Post: {username}</p>;
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/api/link/user/${username}`)
+      .then((response) => {
+        //console.log(response.data);
+        setRes(response.data);
+      })
+      .catch((error) => {
+        console.log('WTF');
+      });
+  }, [username]);
+
+  return (
+    <div>
+      {res.map((link) => (
+        <div key={link.linkId}>{link.url}</div>
+      ))}
+    </div>
+  );
 };
 
 export default Post;
