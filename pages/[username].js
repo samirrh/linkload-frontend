56aba2a560074ee8b713ad53615f3213fab05366
samirrh <samirhaque808@gmail.com>
2021-11-15 22:57:38 -0500

    link boxes

--- pages/[username].js
+++ pages/[username].js
@@ -6,7 +6,7 @@ const Post = () => {
   const router = useRouter();
   const { username } = router.query;
   const [res, setRes] = useState([]);
-  const [user, setUser] = useState('');
+  const [letter, setLetter] = useState('');
 
   useEffect(() => {
     axios
@@ -14,6 +14,7 @@ const Post = () => {
       .then((response) => {
         //console.log(response.data);
         setRes(response.data);
+        setLetter(username.charAt(0));
       })
       .catch((error) => {
         console.log('WTF');
@@ -22,11 +23,33 @@ const Post = () => {
 
   return (
     <div>
-      <div>{res[0].userName}</div>
-      <div>links</div>
-      {res.map((link) => (
-        <div key={link.linkId}>{link.url}</div>
-      ))}
+      <div className="mt-10">
+        <div className="flex justify-center">
+          <div className="rounded-full h-24 w-24 flex items-center justify-center bg-red-300 font-bold text-white text-5xl grid">
+            {letter}
+          </div>
+        </div>
+        <div className="flex justify-center">{username}</div>
+      </div>
+      <div className="grid grid-cols-1 gap-4 p-10">
+        {res.map((link) => (
+          <div
+            className="bg-red-300 grid lg:grid-cols-4 rounded-lg p-3 mb-1 place-items-center"
+            key={link.linkId}
+          >
+            <div className="grid grid-cols-2">
+              <div className="m-1">{link.linkName}</div>
+              <div className="m-1">Views: {link?.views}</div>
+            </div>
+            <div className="m-1">
+              <a href={link.url}>{link.url}</a>
+            </div>
+            <button className="bg-red-500 w-32 hover:bg-red-400 text-white font-bold py-1 px-2 border-b-4 border-red-700 hover:border-red-500 rounded-full m-1">
+              Description
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
