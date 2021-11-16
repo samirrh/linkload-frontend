1fd6205084f065872d75d47633c7f3123b37ffe6
samirrh <samirhaque808@gmail.com>
2021-11-15 23:22:20 -0500

    modal

--- pages/[username].js
+++ pages/[username].js
@@ -1,12 +1,14 @@
 import { useRouter } from 'next/router';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import Modal from '../components/Modal';
 
 const Post = () => {
   const router = useRouter();
   const { username } = router.query;
   const [res, setRes] = useState([]);
   const [letter, setLetter] = useState('');
+  const [showDescription, setShowDescription] = useState(false);
 
   useEffect(() => {
     axios
@@ -29,7 +31,7 @@ const Post = () => {
             {letter}
           </div>
         </div>
-        <div className="flex justify-center">{username}</div>
+        <div className="flex justify-center mt-2 text-2xl">{username}</div>
       </div>
       <div className="grid grid-cols-1 gap-4 p-10">
         {res.map((link) => (
@@ -37,19 +39,45 @@ const Post = () => {
             className="bg-red-300 grid lg:grid-cols-4 rounded-lg p-3 mb-1 place-items-center"
             key={link.linkId}
           >
-            <div className="grid grid-cols-2">
-              <div className="m-1">{link.linkName}</div>
-              <div className="m-1">Views: {link?.views}</div>
+            <div className="m-1">{link.linkName}</div>
+            <div className="m-1 grid grid-cols-2">
+              {link?.views}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                />
+              </svg>
             </div>
+
             <div className="m-1">
               <a href={link.url}>{link.url}</a>
             </div>
-            <button className="bg-red-500 w-32 hover:bg-red-400 text-white font-bold py-1 px-2 border-b-4 border-red-700 hover:border-red-500 rounded-full m-1">
+            <button
+              className="bg-red-500 w-32 hover:bg-red-400 text-white font-bold py-1 px-2 border-b-4 border-red-700 hover:border-red-500 rounded-full m-1"
+              onClick={() => setShowDescription(true)}
+            >
               Description
             </button>
           </div>
         ))}
       </div>
+
+      {showDescription && <Modal></Modal>}
     </div>
   );
 };
