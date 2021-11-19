59cf02eb92e578c71fb1e0dd53cd5acd3668b0b2
samirrh <samirhaque808@gmail.com>
2021-11-18 22:25:39 -0500

    login

--- pages/admin/login.js
+++ pages/admin/login.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 import Link from 'next/link';
+import axios from 'axios';
 
 function login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+  const login = () => {
+    axios
+      .post(`http://localhost:8080/api/auth/login`, {
+        username: String(username),
+        password: String(password),
+      })
+      .then((res) => {
+        const data = res.data;
+        if (typeof window !== 'undefined') {
+          localStorage.setItem('authToken', data.authenticationToken);
+          localStorage.setItem('username', data.username);
+          localStorage.setItem('refreshToken', data.refreshToken);
+          localStorage.setItem('expiresAt', data.expiresAt);
+        }
+        //router.push('/admin/sent');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <>
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -14,7 +39,7 @@ function login() {
               Or <Link href="/admin/signup">Sign Up for an Account!</Link>
             </p>
           </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
+          <div className="mt-8 space-y-6">
             <div className="rounded-md shadow-sm -space-y-px">
               <div>
                 <label htmlFor="email-address" className="sr-only">
@@ -24,10 +49,11 @@ function login() {
                   id="username"
                   name="name"
                   type="name"
-                  autoComplete="email"
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Username"
+                  onChange={(e) => setUsername(e.target.value)}
+                  value={username}
                 />
               </div>
               <div>
@@ -39,6 +65,8 @@ function login() {
                   name="password"
                   type="password"
                   autoComplete="current-password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Password"
@@ -48,7 +76,7 @@ function login() {
 
             <div>
               <button
-                type="submit"
+                onClick={login}
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 <span className="absolute right-0 inset-y-0 flex items-center pr-3">
@@ -70,7 +98,7 @@ function login() {
                 Log In
               </button>
             </div>
-          </form>
+          </div>
         </div>
       </div>
     </>
