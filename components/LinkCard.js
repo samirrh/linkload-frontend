a08d25176eed512f4686f7e542b7c360dcc990bd
samirrh <samirhaque808@gmail.com>
2021-11-25 01:21:45 -0500

    edit and delete link on admin page formatting

--- components/LinkCard.js
+++ components/LinkCard.js
@@ -1,17 +1,64 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { ModalContext } from '../context/ModalContext';
-const LinkCard = ({ linkId, views, url, linkName, description }) => {
+const LinkCard = ({
+  linkId,
+  views,
+  url,
+  linkName,
+  description,
+  deleteLink,
+  updateLink,
+  isLoggedIn,
+}) => {
   const { isOpen, setIsOpen, setLinkName, setLinkDescription } =
     useContext(ModalContext);
+
+  let gridNum = isLoggedIn ? 6 : 4;
   return (
-    <div>
-      <div
-        className="bg-indigo-300 grid lg:grid-cols-4 rounded-lg p-3 mb-1 place-items-center"
-        key={linkId}
+    <div
+      className={`bg-indigo-300 grid lg:grid-cols-${gridNum} rounded-lg p-3 mb-1 place-items-center`}
+      key={linkId}
+    >
+      <div className="m-1">{linkName}</div>
+      <div className="m-1 grid grid-cols-2">
+        {views}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+          />
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+          />
+        </svg>
+      </div>
+
+      <div className="m-1">
+        <a href={url}>{url}</a>
+      </div>
+      <button
+        className="bg-purple-500 w-32 hover:bg-purple-400 text-white font-bold py-1 px-2 border-b-4 border-purple-700 hover:border-purple-500 rounded-full m-1"
+        onClick={() => {
+          setLinkName(linkName);
+          setLinkDescription(description);
+          setIsOpen(true);
+        }}
       >
-        <div className="m-1">{linkName}</div>
-        <div className="m-1 grid grid-cols-2">
-          {views}
+        Description
+      </button>
+      {isLoggedIn && (
+        <>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-6 w-6"
@@ -23,31 +70,25 @@ const LinkCard = ({ linkId, views, url, linkName, description }) => {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth={2}
-              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
             />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth={2}
-              d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+              d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
             />
           </svg>
-        </div>
-
-        <div className="m-1">
-          <a href={url}>{url}</a>
-        </div>
-        <button
-          className="bg-purple-500 w-32 hover:bg-purple-400 text-white font-bold py-1 px-2 border-b-4 border-purple-700 hover:border-purple-500 rounded-full m-1"
-          onClick={() => {
-            setLinkName(linkName);
-            setLinkDescription(description);
-            setIsOpen(true);
-          }}
-        >
-          Description
-        </button>
-      </div>
+        </>
+      )}
     </div>
   );
 };
--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,6 +1,16 @@
 import axios from 'axios';
+import Modal from '../../components/Modal';
+import LinkCard from '../../components/LinkCard';
+import { useEffect, useState, useContext } from 'react';
+import { useRouter } from 'next/router';
+import { ModalContext } from '../../context/ModalContext';
 
 const index = () => {
+  const router = useRouter();
+  const [res, setRes] = useState(Array);
+  const [letter, setLetter] = useState('');
+  const { isOpen } = useContext(ModalContext);
+
   const createLink = () => {
     if (typeof window !== 'undefined') {
       var token = localStorage.getItem('authToken');
@@ -70,26 +80,83 @@ const index = () => {
       });
   };
 
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const username = localStorage.getItem('username');
+    }
+    axios
+      .get(`http://localhost:8080/api/link/user/${username}`)
+      .then((response) => {
+        console.log(response.data);
+        setRes(response.data);
+        setLetter(username.charAt(0));
+      })
+      .catch((error) => {
+        console.log('WTF');
+      });
+  }, []);
+
   return (
-    <div>
-      <button
-        className="py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 "
-        onClick={() => createLink()}
-      >
-        NEW LINK
-      </button>
-      <button
-        className="py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 "
-        onClick={() => deleteLink()}
-      >
-        DELETE LINK
-      </button>
-      <button
-        className="py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 "
-        onClick={() => updateLink()}
-      >
-        UPDATE LINK
-      </button>
+    <div className="flex grid-cols-2">
+      <div className="mx-4 mb-4">
+        <div className="">
+          <div className="flex justify-center mb-4 mt-8">
+            <div className="rounded-full h-24 w-24 flex items-center justify-center bg-indigo-700 font-bold text-white text-5xl grid">
+              {letter}
+            </div>
+          </div>
+          <div className="grid grid-cols-1 px-4 ">
+            <div className="bg-indigo-300 grid lg:grid-cols-3 gap-2 rounded-lg p-3 mb-1 place-items-center">
+              <button
+                className="py-2 px-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 "
+                onClick={() => createLink()}
+              >
+                NEW LINK
+              </button>
+              <button
+                className="py-2 px-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 "
+                onClick={() => deleteLink()}
+              >
+                DELETE LINK
+              </button>
+              <button
+                className="py-2 px-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 "
+                onClick={() => updateLink()}
+              >
+                UPDATE LINK
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mx-4 mb-4 mt-7">
+        <div className="">
+          <div className="grid grid-cols-1 gap-4 ">
+            <div className="bg-purple-300 grid lg:grid-cols-4 rounded-lg p-3 mb-1 place-items-center h-24">
+              <div className="m-1">asfgsf</div>
+              <button
+                className="bg-purple-500 w-32 hover:bg-purple-400 text-white font-bold py-1 px-2 border-b-4 border-purple-700 hover:border-purple-500 rounded-full m-1"
+                onClick={() => {}}
+              >
+                Description
+              </button>
+            </div>
+            {res.map((link) => (
+              <LinkCard
+                key={link?.linkId}
+                linkId={link?.linkId}
+                views={link?.views}
+                url={link?.url}
+                linkName={link?.linkName}
+                description={link?.description}
+                isLoggedIn={true}
+              />
+            ))}
+          </div>
+
+          {isOpen && <Modal></Modal>}
+        </div>
+      </div>
     </div>
   );
 };
