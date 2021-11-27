31f249d0b66946db645ed9cccc59d362026ba00e
samirrh <samirhaque808@gmail.com>
2021-11-26 22:38:05 -0500

    delete and edit buttons

--- components/LinkCard.js
+++ components/LinkCard.js
@@ -13,7 +13,7 @@ const LinkCard = ({
   const { isOpen, setIsOpen, setLinkName, setLinkDescription } =
     useContext(ModalContext);
 
-  let gridNum = isLoggedIn ? 6 : 4;
+  let gridNum = isLoggedIn ? 5 : 4;
   return (
     <div
       className={`bg-indigo-300 grid lg:grid-cols-${gridNum} rounded-lg p-3 mb-1 place-items-center`}
@@ -48,7 +48,7 @@ const LinkCard = ({
         <a href={url}>{url}</a>
       </div>
       <button
-        className="bg-purple-500 w-32 hover:bg-purple-400 text-white font-bold py-1 px-2 border-b-4 border-purple-700 hover:border-purple-500 rounded-full m-1"
+        className="bg-purple-500 w-32 hover:bg-purple-400 text-white font-bold py-1 px-2 border-b-4 my-1 border-purple-700 hover:border-purple-500 rounded-full"
         onClick={() => {
           setLinkName(linkName);
           setLinkDescription(description);
@@ -58,40 +58,44 @@ const LinkCard = ({
         Description
       </button>
       {isLoggedIn && (
-        <>
-          <button onClick={() => deleteLink(linkId)}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-              />
-            </svg>
-          </button>
-          <button onClick={updateLink}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-              />
-            </svg>
-          </button>
-        </>
+        <div className="flex gap-6">
+          <div className="rounded-full h-10 w-10 flex items-center justify-center bg-indigo-700 font-bold text-white">
+            <button onClick={() => deleteLink(linkId)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className="rounded-full h-10 w-10 flex items-center justify-center bg-indigo-700 font-bold text-white">
+            <button onClick={updateLink}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
       )}
     </div>
   );
